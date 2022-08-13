d6238e99f67da439481f8c36c0b876a5d54542a0
Jae Park <jp1004.1040@gmail.com>
2022-08-13 12:54:23 -0400

    Replaced outer Column with CustomScrollView

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,12 +19,14 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // resizeToAvoidBottomInset: false,
-      body: Container(
+      body: Padding(
         padding: defaultPadding,
         child: CustomScrollView(
+          // physics: ClampingScrollPhysics(parent: NeverScrollableScrollPhysics()),
           slivers: [
-            SliverToBoxAdapter(
-              child: const Padding(
+            const SliverToBoxAdapter(
+              child: Padding(
+                // padding: EdgeInsets.symmetric(vertical: 50),
                 padding: EdgeInsets.symmetric(vertical: 50),
                 child: Center(
                   child: SizedBox.square(
@@ -37,77 +39,80 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
             ),
-            SliverToBoxAdapter(
+            SliverFillRemaining(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Welcome Back!',
-                    style: Get.textTheme.displayLarge,
-                  ),
-                  Text(
-                    'Login with your email',
-                    style: Get.textTheme.displayMedium,
-                  ),
-                  const SizedBox(height: 30.0),
-                  AutofillGroup(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        EmailInputField(
-                          controller: ac.emailController,
-                          textInputAction: TextInputAction.next,
-                        ),
-                        const SizedBox(height: 10.0),
-                        PasswordInputField(
-                          controller: ac.passwordController,
-                          textInputAction: TextInputAction.done,
-                          autoFillHints: AutofillHints.password,
-                          confirmPassword: false,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              alignment: Alignment.centerRight,
-                              child: TextButton(
-                                onPressed: () => Get.toNamed("/forgot"),
-                                child: Text(
-                                  'Forgot Password?',
-                                  style: Get.textTheme.titleSmall!.copyWith(color: primaryColor),
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Welcome Back!',
+                        style: Get.textTheme.displayLarge,
+                      ),
+                      Text(
+                        'Login with your email',
+                        style: Get.textTheme.displayMedium,
+                      ),
+                      const SizedBox(height: 30.0),
+                      AutofillGroup(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            EmailInputField(
+                              controller: ac.emailController,
+                              textInputAction: TextInputAction.next,
+                            ),
+                            const SizedBox(height: 10.0),
+                            PasswordInputField(
+                              controller: ac.passwordController,
+                              textInputAction: TextInputAction.done,
+                              autoFillHints: AutofillHints.password,
+                              confirmPassword: false,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  alignment: Alignment.centerRight,
+                                  child: TextButton(
+                                    onPressed: () => Get.toNamed("/forgot"),
+                                    child: Text(
+                                      'Forgot Password?',
+                                      style: Get.textTheme.titleSmall!.copyWith(color: primaryColor),
+                                    ),
+                                  ),
                                 ),
-                              ),
+                              ],
+                            ),
+                            const SizedBox(height: 30.0),
+                            ElevatedButton(
+                              onPressed: () => AuthController.instance.login(),
+                              child: const Text('Log In'),
                             ),
                           ],
                         ),
-                        const SizedBox(height: 30.0),
-                        ElevatedButton(
-                          onPressed: () => AuthController.instance.login(),
-                          child: const Text('Log In'),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SliverFillRemaining(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: Get.textTheme.titleSmall,
-                  ),
-                  TextButton(
-                    onPressed: () => Get.toNamed("/signup/1"),
-                    child: Text(
-                      "Create Account",
-                      style: Get.textTheme.titleSmall!.copyWith(
-                        color: primaryColor,
                       ),
-                    ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: Get.textTheme.titleSmall,
+                      ),
+                      TextButton(
+                        onPressed: () => Get.toNamed("/signup/1"),
+                        child: Text(
+                          "Create Account",
+                          style: Get.textTheme.titleSmall!.copyWith(
+                            color: primaryColor,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
