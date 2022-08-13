95bc1c418f8bc4fa945cb3d67b0da40da4b64d4c
Jae Park <jp1004.1040@gmail.com>
2022-08-13 03:18:10 -0400

    Profile complete and Sign up almost complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -6,17 +7,19 @@ import 'package:whapp/firebase_options.dart';
 import 'package:whapp/pages/login_page.dart';
 import 'controllers/auth_controller.dart';
 import 'firebase_options.dart';
+import 'models/member.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform) //
-      .then((value) => Get.put(AuthController()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); //
+  // .then((value) => Get.put(AuthController()));
 
   // Prevent landscape mode
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(const MyApp());
+  // runApp(const MyApp());
+  runApp(const Test());
 }
 
 class MyApp extends StatelessWidget {
@@ -32,4 +35,58 @@ class MyApp extends StatelessWidget {
       home: LoginPage(),
     );
   }
+}
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+      alignment: Alignment.center,
+      child: const ElevatedButton(
+        child: const Text("Add member"),
+        onPressed: _test,
+      ),
+    ));
+  }
+}
+
+_test() async {
+  var db = FirebaseFirestore.instance;
+
+  final ref = db
+      .collection("members") //
+      .doc('nPeBSfxshYUfwwti6T1BxLLa6MA2')
+      .withConverter(
+        fromFirestore: Member.fromFirestore,
+        toFirestore: (Member m, _) => m.toFirestore(),
+      );
+
+  final member = Member(
+    uid: "nPeBSfxshYUfwwti6T1BxLLa6MA2",
+    role: 1,
+    firstName: "Jae",
+    lastName: "Park",
+    studentId: "1157273",
+    homeroom: "Mrs. Ryan",
+    gradeLevel: "senior",
+    hispanic: false,
+    ethnicities: ["asian"],
+    cellPhone: "7708834212",
+    address: "4153 Moncure Drive, Lilburn, GA, 30047",
+    primaryParent: Parent(
+      firstName: "Eun",
+      lastName: "Park",
+      emailAddress: "pk2000love@gmail.com",
+      cellPhone: "4045506826",
+      work: "medside",
+    ),
+    tShirtSize: "large",
+    tShirtReceived: false,
+    duesPaid: false,
+  );
+
+  await ref.set(member);
 }
