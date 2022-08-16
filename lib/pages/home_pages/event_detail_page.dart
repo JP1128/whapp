442d250fdb298c7f4c616d4e6a169a5110b65a3d
Jae Park <jp1004.1040@gmail.com>
2022-08-16 01:45:44 -0400

    Creating event detail page

--- lib/pages/home_pages/event_detail_page.dart
+++ lib/pages/home_pages/event_detail_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:whapp/constants/constants.dart';
+import 'package:whapp/constants/theme.dart';
+import 'package:whapp/controllers/auth_controller.dart';
 import 'package:whapp/models/events/event.dart';
 
 class EventDetailPage extends StatefulWidget {
@@ -16,6 +20,7 @@ class EventDetailPage extends StatefulWidget {
 }
 
 class _EventDetailPageState extends State<EventDetailPage> {
+  final AuthController _ac = Get.find();
   late Event _event;
 
   @override
@@ -26,6 +31,36 @@ class _EventDetailPageState extends State<EventDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(() => Scaffold(
+          appBar: AppBar(
+            actions: [
+              if (_ac.member.value != null && _ac.member.value!.role < 3)
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit_outlined),
+                ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Container(
+              padding: defaultPadding,
+              child: Column(
+                children: [
+                  if (_event.boardOnly)
+                    Text(
+                      "Board member only",
+                      style: Get.textTheme.bodyMedium!.copyWith(
+                        color: palette[6],
+                      ),
+                    ),
+                  Text(
+                    _event.title,
+                    style: Get.textTheme.displayMedium,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
